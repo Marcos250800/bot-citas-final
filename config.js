5de2074e05828f626bac727e2eb07ed875256333
Marcos250800 <elkchy25@gmail.com>
2025-11-23 15:09:05 +0000

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    base44ApiUrl: 'https://www.citaconsular.es/es/hosteds/widgetdefault/25b6cfa9f112aef4ca19457abc237f7ba',
+    
+    // TUS DATOS
+    emailUser: 'starlin25000@gmail.com', 
+    emailPass: 'tsvi jwtx vmsn genk', // <--- ¡IMPORTANTE!
+    emailDestino: 'starlin25000@gmail.com'
+};
