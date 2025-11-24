fa207fe400128eb579f65455c28f2b5b65b1c8fe
Marcos250800 <elkchy25@gmail.com>
2025-11-24 20:43:50 +0000

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-module.exports = {
-    // No se usa intervalo aquí, lo maneja GitHub, pero déjalo por si acaso
-    checkInterval: 0, 
+mmodule.exports = {
+    // URL DEL MINISTERIO (Entrada humana para evitar bloqueo)
     base44ApiUrl: 'https://www.exteriores.gob.es/Consulados/lahabana/es/ServiciosConsulares/Paginas/index.aspx?scco=Cuba&scd=166&scca=Legalizaci%c3%b3n+o+Apostilla.+Compulsa+y+Registro&scs=Legalizaci%c3%b3n+y+apostilla+de+la+Haya',
+    
+    // TUS DATOS
     emailUser: 'starlin25000@gmail.com', 
-    emailPass: 'tsvi jwtx vmsn genk', 
+    emailPass: 'tsvi jwtx vmsn genk', // <--- ¡TU CLAVE REAL!
     emailDestino: 'starlin25000@gmail.com'
 };
