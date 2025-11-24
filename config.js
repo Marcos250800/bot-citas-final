5e0bb87bed4f72621fcb4e18c50db5e29f92365f
Marcos250800 <elkchy25@gmail.com>
2025-11-24 02:24:35 +0000

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    base44ApiUrl: 'https://www.citaconsular.es/es/hosteds/widgetdefault/25b6cfa9f112aef4ca19457abc237f7ba',
-    
-    // TUS DATOS
+    // No se usa intervalo aquí, lo maneja GitHub, pero déjalo por si acaso
+    checkInterval: 0, 
+    base44ApiUrl: 'https://www.exteriores.gob.es/Consulados/lahabana/es/ServiciosConsulares/Paginas/index.aspx?scco=Cuba&scd=166&scca=Legalizaci%c3%b3n+o+Apostilla.+Compulsa+y+Registro&scs=Legalizaci%c3%b3n+y+apostilla+de+la+Haya',
     emailUser: 'starlin25000@gmail.com', 
-    emailPass: 'tsvi jwtx vmsn genk', // <--- ¡IMPORTANTE!
+    emailPass: 'tsvi jwtx vmsn genk', 
     emailDestino: 'starlin25000@gmail.com'
 };
